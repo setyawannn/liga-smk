04ebb27bed38005a1422eedb8467607ba4f73371
setyawannn <prayogasetyawan.adi@gmail.com>
2023-12-05 22:24:54 +0700

    (add): fix navbar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/Components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,45 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-import ArrowBottom from "../../public/assets/icon-arrow-bottom.svg";
+import ArrowBottom from "../Assets/icon-arrow-bottom.svg";
+import { ReactSVG } from "react-svg";
+import Image from "next/image";
+
+const NavbarList = [
+  { id: 1, title: "Home", path: "/", dropdown: true },
+  { id: 2, title: "About Us", path: "/", dropdown: true },
+  { id: 3, title: "Blog", path: "/", dropdown: true },
+  { id: 4, title: "Kontak", path: "/" },
+];
 
 const Navbar = () => {
   return (
-    <div className="max-w-5xl mx-auto">
-      <div className="flex items-center justify-between py-6">
+    <div className="bg-white py-6">
+      <div className="flex items-center justify-between max-w-5xl mx-auto">
         <div>
-          <h1>Liga SMK</h1>
+          <Image
+            src={"/Assets/logo-ligasmk-dark.png"}
+            alt="Logo Liga SMK"
+            width={120}
+            height={120}
+            quality={100}
+          />
         </div>
         <div className="flex gap-8">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/"}>About Us</Link>
-          <Link href={"/"}>Kelas</Link>
-          <Link href={"/"}>Blog</Link>
-          <Link href={"/"}>Kontak</Link>
+          {NavbarList.map((item) => (
+            <Link
+              key={item.id}
+              href={"/"}
+              className="flex items-center gap-x-3"
+            >
+              {item.title}{" "}
+              {item.dropdown ? (
+                <ReactSVG src="/Assets/icon-arrow-bottom.svg" />
+              ) : (
+                ""
+              )}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
